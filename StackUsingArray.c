f09321a1cc728caf068304e24b2015155c7d5767
Jagdamba Tripathi <jagdtri2003@gmail.com>
2022-10-15 08:04:17 +0530

    added switch statement

--- StackUsingArray.c
+++ StackUsingArray.c
@@ -11,7 +11,42 @@ int Stack[Max],top=-1;
 
 int main(){
 
-    
+    int ch,cnt,key;
+    do{
+        printf("1.Push \n2.Pop \n3.Peek \n4.isEmpty \n5.isFull \nEnter Your Choice : ");
+        scanf("%d",&ch);
+        switch (ch)
+        {
+        case 1:
+            printf("Enter the value : ");
+            scanf("%d",&key);
+            push(key);
+            break;
+        case 2:
+            pop();
+            break;
+        case 3:
+            printf("Enter the value : ");
+            scanf("%d",&key);
+            push(key);
+            break;
+        case 4:
+            printf("Enter the value : ");
+            scanf("%d",&key);
+            push(key);
+            break;
+        case 5:
+            printf("Enter the value : ");
+            scanf("%d",&key);
+            push(key);
+            break;       
+        default:
+            printf("Invalid input !!");
+            break;
+        }
+        printf("Do you want to continue(0/1) :");
+        scanf("%d",&cnt);
+    }while(cnt);
 
 
 
