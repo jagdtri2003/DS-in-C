d86b1329e5f7efcfe3cee6a1cb7658ad932ffd66
Jagdamba Tripathi <jagdtri2003@gmail.com>
2022-10-15 07:55:27 +0530

    Created

---
+++ StackUsingArray.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+#define Max 10
+
+void push(int data);
+int pop();
+int peek();
+bool isFull();
+bool isEmpty();
+int Stack[Max],top=-1;
+
+int main(){
+
+    
+
+
+
+    return 0;
+}
