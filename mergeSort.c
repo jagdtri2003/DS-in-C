2cff4ee628a5d25c4c8a3cce85c75f10e7a23ba3
Jagdamba Tripathi <jagdtri2003@gmail.com>
2022-10-15 07:28:56 +0530

    compact

--- mergeSort.c
+++ mergeSort.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 #define inf 9999
-
 void merge_sort(int arr[],int p,int r);
 void merge(int arr[],int p,int q,int r);
-
 int main(){
-
     int n,i;
     printf("Enter the number of elements : " );
     scanf("%d",&n);
@@ -14,9 +11,7 @@ int main(){
     for (i=0;i<n;i++){
         scanf("%d",&a[i]);
     }
-
     merge_sort(a,0,n-1);
-
     printf("Sorted Array is : ");
     for (i=0;i<n;i++){
         printf("%d\t",a[i]);
@@ -35,16 +30,12 @@ void merge_sort(int arr[],int p,int r){
 void merge(int arr[],int p,int q,int r){
 
     int n1,n2,i,j,k;
-
     n1=q-p+1;
     n2=r-p;
-
     int L[n1],R[n2];
-
     for (i=0;i<n1;i++){
         L[i]=arr[p+i];
     }
-
     for (j=0;j<n1;j++){
         R[j]=arr[q+j+1];
     }
