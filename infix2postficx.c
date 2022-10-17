69d99b073fb469854503813b20d440772d0b452d
Jagdamba Tripathi <54079135+jagdtri2003@users.noreply.github.com>
2022-10-17 15:56:56 +0530

    Create infix2postficx.c

---
+++ infix2postficx.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <math.h>
+
+char Stack[20];
+int top=-1;
+void push(char);
+char pop();
+int priority(char);
+
+
+int main(){
+	
+	char infix[30],postfix[30];
+	char *in,*post,x,y;
+	printf("Enter the Infix Expression : ");
+	scanf("%s",infix);
+	in=infix;
+	post=postfix;
+	
+	while((x=*in)!='\0'){
+	
+		if (x=='(')
+			push(x);
+		else if (isalnum(x)){
+		
+			*post=x;
+			post++;
+		}
+		else if (x==')'){
+		
+			while((y=pop())!='('){
+				
+				*post=pop();
+				post++;		
+			}
+				
+		}
+		
+		else{
+		
+			if (priority(x)>priority(Stack[top]))
+				push(x);
+			else{
+			
+				while(priority(x)<=priority(Stack[top])){
+				
+					*post=pop();
+					post++;
+						
+				}
+				push(x);			
+			}		
+		}
+	in++;
+	}
+	
+	while(top!=-1){
+	
+		*post=pop();
+		post++;
+
+	}
+	
+	printf("Postfix Expression is %s ",postfix);
+	/*
+	printf("Result Calculation :");
+	
+	post=postfix;
+	top=-1;
+	while((x=*post!)='\0'){
+	
+		if (x=='*')
+			push(pop()*pop());
+		else if (x=='/')
+			push(pop()/pop());
+		else if (x=='^')
+			push(pow(pop(),pop()));
+		else if (x=='+')
+			push(pop()+pop());
+		else if (x=='-')
+			push(pop()-pop());		
+		else 
+			push(x)
+	}
+	printf("Result : %s ",pop());
+  
+  */
+  
+	return 0;
+}
+
+void push(char x){
+	
+	Stack[++top]=x;
+
+}
+
+char pop(){
+
+	if (top==-1)
+		return -1;
+	return Stack[top--];
+
+}
+
+int priority(char x){
+
+	if (x=='(')
+		return 0;
+	else if (x=='+' || x=='-')
+		return 1;
+	else if (x=='*' || x=='/')
+		return 2;
+	else if (x=='^')
+		return 3;
+
+}
